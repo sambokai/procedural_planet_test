31c60b1dadd67f489ffc12c6e4d6c5cde0b37dd3
Sam Bokai <sam.bokai@student.HTW-Berlin.de>
2017-04-25 11:49:25 +0200

    Cleanup;
    Use SLagues flat shading implementation; proc-terrain mesh is currently not seamless;

--- planet_test/Assets/Editor/MapGeneratorEditor.cs
+++ planet_test/Assets/Editor/MapGeneratorEditor.cs
@@ -1,26 +1,21 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEditor;
-using UnityEngine;
-
 
 [CustomEditor (typeof (MapGenerator))]
 public class MapGeneratorEditor : Editor {
 
-    public override void OnInspectorGUI()
-    {
-        MapGenerator mapGen = (MapGenerator)target;
+	public override void OnInspectorGUI() {
+		MapGenerator mapGen = (MapGenerator)target;
 
-        if (DrawDefaultInspector())
-        {
-            if (mapGen.autoUpdate)
-            {
-                mapGen.GenerateMap();
-            }
-        }
+		if (DrawDefaultInspector ()) {
+			if (mapGen.autoUpdate) {
+				mapGen.DrawMapInEditor ();
+			}
+		}
 
-        if (GUILayout.Button("Generate"))
-        {
-            mapGen.GenerateMap();
-        }
-    }
+		if (GUILayout.Button ("Generate")) {
+			mapGen.DrawMapInEditor ();
+		}
+	}
 }
